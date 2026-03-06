3c155221381ffbe0ce5929879fd450ebf636a5b2
mohith088 <kaviythamizhan@gmail.com>
2026-03-06 09:41:31 +0530

    Mohith Added UC9

--- PalindroneChecker/src/PalindroneCheckerApp.java
+++ PalindroneChecker/src/PalindroneCheckerApp.java
@@ -1,32 +1,25 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
-public class PalindroneCheckerApp{
+public class PalindroneCheckerApp {
 
-    public static boolean checkPalindrome(String input) {
-        LinkedList<Character> list = new LinkedList<>();
+    private static boolean check(String s, int start, int end) {
+        if (start >= end)
+            return true;
 
-        // Add characters to linked list
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
+        if (s.charAt(start) != s.charAt(end))
+            return false;
 
-        // Compare first and last
-        while (list.size() > 1) {
-            if (!list.removeFirst().equals(list.removeLast())) {
-                return false;
-            }
-        }
-        return true;
+        return check(s, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input: ");
         String input = sc.nextLine();
 
-        boolean result = checkPalindrome(input);
+        boolean result = check(input, 0, input.length() - 1);
 
         System.out.println("Is Palindrome?: " + result);
     }
