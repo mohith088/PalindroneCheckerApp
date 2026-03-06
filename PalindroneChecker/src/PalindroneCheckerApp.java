33e5e2a39ce6e7bb502325ae7b124f8d56f71332
kaviythamizhan1207 <kaviythamizhan@gmail.com>
2026-03-06 08:57:46 +0530

    Mohith Added UC3

---
+++ PalindroneChecker/src/PalindroneCheckerApp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Palindrome Checker App");
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String text = sc.nextLine();
+
+        String reversed = "";
+
+        // reverse using loop
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+
+        System.out.println("Reversed text: " + reversed);
+
+        if (text.equals(reversed)) {
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Is it a Palindrome? : false");
+        }
+    }
+}
